4d1095004dcc92a05ae6a5aaaffe248c727f05f0
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-05-16 10:24:32 +1200

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
-import ReactDOM from "https://esm.sh/react-dom";
 
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      markdown: "" };
+let text = "";
+let count = 0;
+let caseProperty = document.editor.caseProperty.value;
 
-  }
-  updateMarkdown(markdown) {
-    this.setState({ markdown });
-  }
+function switchCase() {
+  count++;  
+  text += "\n\nconst case" + count + " = () => {\n  return {\n    type: \"" + document.editor.caseProperty.value + "\";\n  }\n};";
+}
 
-  render() {
-    }
-    return /*#__PURE__*/(
-      React.createElement("div", { class: "container" }, /*#__PURE__*/
-      React.createElement("textarea", { id: "editor",
-        value: this.state.markdown,
-        onChange: e => {this.updateMarkdown(e.target.value);
-        } },
-
-      console.log(this.state.markdown)), /*#__PURE__*/
-
-      React.createElement("div", { id: "preview", class: "container", dangerouslySetInnerHTML: { __html: this.state.markdown } }), /*#__PURE__*/
-
-      React.createElement(Output, null)));
-
-
-  }}
-
-
-const Output = (props) => /*#__PURE__*/
-React.createElement("div", null, /*#__PURE__*/
-
-React.createElement("div", null, props.output));
-
-
-
-
-
-ReactDOM.render( /*#__PURE__*/React.createElement(Input, null), document.getElementById("markdown"));
+function printCases() {
+  document.editor.textbox.value += text;
+  text = "";
+  count = 0;
+}
