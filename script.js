4e58c8a5ec3b7ac3fef4093dbe79180bec6c484f
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-05-23 15:47:51 +1200

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
-let text = "";
+let case = "";
 let constant = "";
 let count = 0;
 let caseProperty = document.editor.caseProperty.value;
 
 function switchCase() {
   count++;  
-  text += "\n\nconst case" + count + " = () => {\n  return {\n    type: " + document.editor.caseValue.value.replace(' ', '').toUpperCase() + ";\n  }\n};";
+  case += "\n\nconst case" + count + " = () => {\n  return {\n    type: " + document.editor.caseValue.value.replace(' ', '').toUpperCase() + ";\n  }\n};";
   constant += "\nconst " + document.editor.caseValue.value.replace(' ', '').toUpperCase() + " = \"" + document.editor.caseValue.value.replace(' ', '').toUpperCase() + "\";";
 }
 
 function printCases() {
-  document.editor.textbox.value += constant;
+  document.editor.textbox.value += case;
   text = "";
   count = 0;
+}
+
+function printConstant() {
+  document.editor.textbox.value += constant;
+  constant = "";
 }
