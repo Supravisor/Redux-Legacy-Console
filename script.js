1f0b12e9c50234a8366666355958a0f5dcb637f1
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-06-14 11:15:16 +1200

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 let casesSwitch = "\nconst reducer = (state = defaultState, " + document.editor.reducerAction.value + ") => {\n\n  switch (action.type) {";
 let cases = "";
+let caseVariable = "";
 let constant = "";
 let count = 0;
 
 function switchCase() {
   count++;
   document.editor.textbox.value += "\n    case " + document.editor.caseAction.value.replace(' ', '').toUpperCase() + ":\n      return {\n        " + document.editor.defaultStateKey.value + ": " + document.editor.caseValue.value + "\n      };";
-  cases += "\n\nconst case" + count + " = () => {\n  return {\n    type: " + document.editor.caseAction.value.replace(' ', '').toUpperCase() + ";\n  }\n};";
+  cases += "\n    case " + document.editor.caseAction.value.replace(' ', '').toUpperCase() + ":\n      return {\n        " + document.editor.defaultStateKey.value + ": " + document.editor.caseValue.value + "\n      };";
+  caseVariable += "\n\nconst case" + count + " = () => {\n  return {\n    type: " + document.editor.caseAction.value.replace(' ', '').toUpperCase() + ";\n  }\n};";
   constant += "\nconst " + document.editor.caseAction.value.replace(' ', '').toUpperCase() + " = \"" + document.editor.caseAction.value.replace(' ', '').toUpperCase() + "\";";
 }
 
 function handleMultipleActions() {
-  document.editor.textbox.value = '\nconst defaultState = {\n  ' + document.editor.defaultStateKey.value + ': ' + document.editor.defaultStateValue.value + '\n};\n';
+  document.editor.textbox.value = '\nconst defaultState = {\n  ' + document.editor.defaultStateKey.value + ': ' + document.editor.defaultStateValue.value + '\n};';
   document.editor.textbox.value += constant;
   constant = "";
-  document.editor.textbox.value += "\n    default:\n    return defaultState;        \n  }\n}";
   document.editor.textbox.value += cases;
+  document.editor.textbox.value += "\n    default:\n    return defaultState;        \n  }\n}";
+  document.editor.textbox.value += caseVariable;
   cases = "";
+  caseVariable = "";
   count = 0;
 }
